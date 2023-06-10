b03092ba9ca6f99691a73b42f9aa8ddc30ee1c9c
Arzu.Code <arzuem@code.edu.az>
2023-06-10 07:42:58 +0300

    first commit

---
+++ Geometry/GeometryHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public static class GeometryHelper
+    {
+        public static void BucaqSahesi ()
+        {
+            Console.WriteLine("4 Bucagin sahesini hesablayan metod ");
+        }
+        public static void ucbucaq ()
+        {
+            Console.WriteLine("3 Bucagin sahesini hesablayan metod");
+        }
+        public static void Daire()
+        {
+            Console.WriteLine("Dairenin sahesini hesablayan metod");
+        }
+
+    }
+}
---
+++ Geometry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Geometry
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            GeometryHelper.BucaqSahesi();
+            GeometryHelper.ucbucaq();
+            GeometryHelper.Daire();
+
+
+            
+        }
+    }
+}
